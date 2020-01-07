fcbbea1c0261001f96f0dbfef1040520f9401e94
benjamingomez0 <benjamingomez0@gmail.com>
2020-01-06 19:02:08 -0800

    hackerrank plusMinus solution

--- index.js
+++ index.js
@@ -30,20 +30,46 @@ function aVeryBigSum(ar)
 
 // console.log(aVeryBigSum(bigArr));
 // matrixArr=[ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ]
-matrixArr = [[1,2,3],[4,5,6],[9,8,9 ]]
+// matrixArr = [[1,2,3],[4,5,6],[9,8,9 ]]
 
  
-const diagonalDifference=(arr)=>{
-    let sum1 = 0;
-    let sum2 =0;
-    let n = arr.length-1
-   for(let i =0; i<arr.length;i++)
-   {
-       sum1+=arr[i][i];
-       sum2+= arr[n-i][i]
-   }
-   let diff = Math.abs(sum1-sum2);
-    return diff
-}
+// const diagonalDifference=(arr)=>{
+//     let sum1 = 0;
+//     let sum2 =0;
+//     let n = arr.length-1
+//    for(let i =0; i<arr.length;i++)
+//    {
+//        sum1+=arr[i][i];
+//        sum2+= arr[n-i][i]
+//    }
+//    let diff = Math.abs(sum1-sum2);
+//     return diff
+// }
 
-console.log(diagonalDifference(matrixArr))
+// console.log(diagonalDifference(matrixArr))
+const testArr= [-4,3,-9,0,4,1]
+
+function plusMinus(arr) {
+    let pos = 0;
+    let neg = 0;
+    let z = 0;
+    for(let i = 0; i<arr.length;i++)
+    {
+        if(arr[i]>0)
+        {
+            pos++
+        }
+        else if(arr[i]===0)
+        {
+            z++
+        }
+        else if(arr[i]<0)
+        {
+            neg++
+        }
+    }
+    console.log((pos/arr.length).toFixed(6))
+    console.log((neg/arr.length).toFixed(6))
+    console.log((z/arr.length).toFixed(6))
+}
+plusMinus(testArr)
