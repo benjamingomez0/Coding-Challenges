1d0e3d9c7b104930e16cbb94e18110d6d6e3895a
benjamingomez0 <benjamingomez0@gmail.com>
2020-01-06 18:39:09 -0800

    diagonal difference hackerrank solution

--- index.js
+++ index.js
@@ -28,4 +28,22 @@ function aVeryBigSum(ar)
     return sum;
 }
 
-console.log(aVeryBigSum(bigArr));
+// console.log(aVeryBigSum(bigArr));
+// matrixArr=[ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ]
+matrixArr = [[1,2,3],[4,5,6],[9,8,9 ]]
+
+ 
+const diagonalDifference=(arr)=>{
+    let sum1 = 0;
+    let sum2 =0;
+    let n = arr.length-1
+   for(let i =0; i<arr.length;i++)
+   {
+       sum1+=arr[i][i];
+       sum2+= arr[n-i][i]
+   }
+   let diff = Math.abs(sum1-sum2);
+    return diff
+}
+
+console.log(diagonalDifference(matrixArr))
